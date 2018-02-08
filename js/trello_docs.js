7cffe3d0b2b3959e0fda0ee30317d8d1621a0e32
Luigi Castaldo <luigi.castald@gmail.com>
2018-02-08 15:26:04 +0100

    Removing Intro

--- js/trello_docs.js
+++ js/trello_docs.js
@@ -32,6 +32,7 @@ $(document).ready(function(){
 
 var initDoc=function () {
 	if (!Trello.authorized()) return Trello.authorize(defaultOptions);
+	
 	Trello.get('/members/me',{boards:"open",organizations:"all"}, function(me) {
 		window.myself=me;
 		router();
@@ -43,6 +44,7 @@ var initDoc=function () {
 			$("#view").html("<h1>Connection to Trello API is broken, Please <a href='javascript:reload();'>Reload</a></h1>");
 		}
 	});
+	
 };
 
 var router=function(){
@@ -85,7 +87,7 @@ var listBoards=function(){
 
 	$("#view").empty();
 	var intro="<div class='list info-list'><h2>About Trello2HTML</h2><p>This is an web app to export Trello Boards to HTML, our team uses this to record our progress every month. We do not track or record you any way, and Trello access is read-only. You can host this on any static server. Google Chrome is tested and supported, your mileage may vary with other browsers(Firefox has a bug when downloading).</p><ul><a href='#4d5ea62fd76aa1136000000c'><li>Demo using Trello Development</li></a><a href='trello.zip'><li>Download zipped source</li></a><a href='https://trello.com/board/trello2html/4fb10d0e312c2b226f1eb4a0'><li>Feature Requests and Bug Reports</li></a><a href='http://tianshuohu.diandian.com/post/2012-06-08/Trello-Export-as-html'><li>Blog Article (Chinese/English)</li></a></ul></div>";
-	var template="<h1>{{fullName}} ({{username}})</h1><div id='boardlist'>"+intro+"{{#orgBoards}}<div class='list'><h2>{{name}}</h2><ul>{{#boards}}<a href='#{{id}}' ><li>{{name}}</li></a>{{/boards}}</ul></div>{{/orgBoards}}</div>";
+	var template="<h1>{{fullName}} ({{username}})</h1><div id='boardlist'>"+"{{#orgBoards}}<div class='list'><h2>{{name}}</h2><ul>{{#boards}}<a href='#{{id}}' ><li>{{name}}</li></a>{{/boards}}</ul></div>{{/orgBoards}}</div>";
 	var str=Mustache.render(template,myself);
 	$("#view").html(str);
 	$("#boardlist").masonry({
